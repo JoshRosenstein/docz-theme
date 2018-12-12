3c1cd2782f5b2024ae17b8ff3bb375b17351d96c
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-12-12 01:32:06 -0500

    Fix Theme Toggler

--- src/components/shared/ThemeToggler/index.js
+++ src/components/shared/ThemeToggler/index.js
@@ -1,25 +1,10 @@
-import React,{ Component } from 'react'
+import React from 'react'
 import { Sun, Moon  } from 'react-feather'
-import styled,{css} from 'react-emotion'
-import { styler,getColor ,media,getThemeP} from '@styles/system'
+import styled from '@emotion/styled'
+import { getThemeP} from '@styles/system'
 
 import {ThemeContext} from '@styles/themeToggle'
 
-const linkStyle = p => css`
-  color:${getThemeP('colors.primary')(p)};
-
-  opacity: 0.5;
-  transition: opacity 0.2s;
-  font-size: 15px;
-  font-weight: 400;
-
-  &.active,
-  &:visited,
-  &:hover {
-    color: ${getThemeP('colors.text')(p)};
-    opacity: 1;
-  }
-`
 
 const IconButtonLink = styled('button')`
   display: flex;
